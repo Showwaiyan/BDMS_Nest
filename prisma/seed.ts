aa5016b546b436ddaa3a108eb081de79fabe5e71
Minzo <minzo@gmail.com>
2026-03-03 15:43:26 +0000

    chore: revert to custom prisma output path and CJS module format

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import * as pg from 'pg';
 import * as bcrypt from 'bcryptjs';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './generated/client';
 
 const connectionString = process.env.DATABASE_URL!;
 const pool = new pg.Pool({ connectionString });
